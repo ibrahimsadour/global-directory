0b7621e78e40d5202be206118dfa048b74871622
Ibrahim Sadour <i.m.s.1995@hotmail.com>
2025-07-16 22:54:05 +0200

    filter1

--- resources/views/livewire/business-list.blade.php
+++ resources/views/livewire/business-list.blade.php
@@ -1,22 +1,24 @@
-<x-filters-popup 
-    :categories="$categories" 
-    :governorates="$governorates"
-    :selectedCategory="$selectedCategory"
-    :selectedGovernorate="$selectedGovernorate"
-    :ratingFilter="$ratingFilter"
->
-    {{-- ✅ شريط الأدوات والنتائج --}}
-    @include('business.Livewire.businesses-toolbar', [
-        'businesses' => $businesses, 
-        'view' => $view
-    ])
+<div>
+    <x-filters-popup 
+        :categories="$categories" 
+        :governorates="$governorates"
+        :selectedCategory="$selectedCategory"
+        :selectedGovernorate="$selectedGovernorate"
+        :ratingFilter="$ratingFilter"
+    >
+        {{-- ✅ شريط الأدوات والنتائج --}}
+        @include('business.Livewire.businesses-toolbar', [
+            'businesses' => $businesses, 
+            'view' => $view
+        ])
 
-    @include('business.Livewire.businesses-view', [
-        'businesses' => $businesses, 
-        'view' => $view
-    ])
+        @include('business.Livewire.businesses-view', [
+            'businesses' => $businesses, 
+            'view' => $view
+        ])
 
-    <div class="mt-6">
-        {{ $businesses->links() }}
-    </div>
-</x-filters-popup>
+        <div class="mt-6">
+            {{ $businesses->links() }}
+        </div>
+    </x-filters-popup>
+</div>
